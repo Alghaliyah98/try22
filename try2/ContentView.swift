ae6bf42ed3bcdece3b02e5e9851eb6e9262f3399
alghaliyah <alghaliyah.alghofaili98@gmail.com>
2023-10-08 14:32:58 +0300

    Initial Commit

---
+++ try2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  try2
+//
+//  Created by Alghaliyah Alghofaili on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ try2/try2App.swift
@@ -0,0 +1,17 @@
+//
+//  try2App.swift
+//  try2
+//
+//  Created by Alghaliyah Alghofaili on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct try2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
